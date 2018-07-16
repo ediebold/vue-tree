ab2040d01dc8c3c82dceb7bae032c04f9115270f
ELLIOT DIEBOLD <>
2018-07-16 10:06:29 +1000

    included vuex module in export

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import VueInputAutowidth from 'vue-input-autowidth'
 import 'vue-awesome/icons'
 import Icon from 'vue-awesome/components/Icon.vue'
 
+import VuexTree from './modules/VuexTree.js'
+
 VTree.install = function(Vue){
   Vue.use(VueInputAutowidth)
   Vue.component(Icon.name, Icon)
@@ -16,4 +18,4 @@ if (typeof window !== 'undefined' && window.Vue) {
   window.Vue.use(VTree);
 }
 
-export default VTree
+export default {VTree, VuexTree}
